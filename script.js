ff493d5344473d24706ac2a03474dd7e467ded68
Kinsej <118400668+k1nsej@users.noreply.github.com>
2023-03-19 23:01:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,26 @@ quizForm.addEventListener('submit', (event) => {
 });
 
 function getResult(purpose, size, performance, budget) {
-	// Hier können Sie die Logik für die Auswertung des Tests implementieren
-	// und das passende Apple-Gerät basierend auf den ausgewählten Antworten zurückgeben.
+	let result = '';
+	
+	// Logik für die Auswertung des Tests
+	if (purpose === 'leisure' && size === 'small' && performance === 'basic' && budget === 'low') {
+		result = 'iPad mini';
+	} else if (purpose === 'leisure' && size === 'medium' && performance === 'basic' && budget === 'low') {
+		result = 'iPad';
+	} else if (purpose === 'leisure' && size === 'large' && performance === 'basic' && budget === 'medium') {
+		result = 'iPad Air';
+	} else if (purpose === 'leisure' && size === 'large' && performance === 'basic' && budget === 'high') {
+		result = 'iPad Pro';
+	} else if (purpose === 'leisure' && size === 'large' && performance === 'high' && budget === 'high') {
+		result = 'iPad Pro (M1)';
+	} else if (purpose === 'work' && size === 'medium' && performance === 'basic' && budget === 'medium') {
+		result = 'MacBook Air';
+	} else if (purpose === 'work' && size === 'medium' && performance === 'high' && budget === 'high') {
+		result = 'MacBook Pro (M1)';
+	} else if (purpose === 'work' && size === 'large' && performance === 'high' && budget === 'high') {
+		result = 'iMac (M1)';
+	}
+	
+	return result;
 }
